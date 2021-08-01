3931f947b90cd257ff3902ab474a91ac5d5b6da7
carlos <carlosAndrade@users.noreply.github.com>
2021-07-31 21:55:13 -0500

    actualizacion

--- dist/index.js
+++ dist/index.js
@@ -14,7 +14,8 @@ server.app.use(body_parser_1.default.json());
 //ruta aceeso para el user
 server.app.use('/user', usuario_1.default);
 //conexion con mongoDB utilizando mongose
-mongoose_1.default.connect('mongodb://localhost:27017/tienda', {
+//mongoose.connect('mongodb://localhost:27017/tienda',{
+mongoose_1.default.connect('mongodb+srv://cocoa:Programar.1234@cluster0.azijh.mongodb.net/tienda', {
     useNewUrlParser: true, useCreateIndex: true
 }, function (err) {
     if (err)
