0de92e8abb9450935d058214ec95662418153905
carlos <carlosAndrade@users.noreply.github.com>
2021-08-01 14:39:20 -0500

    deploy

--- models/user.models.ts
+++ models/user.models.ts
@@ -23,12 +23,21 @@ const usuarioSchema= new Schema <IUsuario>({
 
 });
 //metodo de comparación
+usuarioSchema.method('compararPassword', function(password: string =''): boolean{
+if(bcrypt.compareSync(password, this.password)){
+    return true;
+}
+else{
+    return false;
+}
+});
+
 export interface IUsuario extends Document{
-nombre:String;
-email: String;
-password: String;
-avatar: String;
-compararPassword(password: string): boolean;
+nombre:string;
+email: string;
+password: string;
+avatar: string;
+compararPassword(password: String): boolean;
 }
 
 export const Usuario= model<IUsuario>('Usuario',usuarioSchema);
